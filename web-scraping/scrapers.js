bdfc5d9df88006993e31ed2633e87fc40c7c57d1
Alette Ng Xin Ling <xinlingng@Alettes-MacBook-Air.local>
2023-01-16 12:10:28 -0500

    web-scarping if statements for industry

--- web-scraping/scrapers.js
+++ web-scraping/scrapers.js
@@ -151,7 +151,72 @@ async function sendToDatabase(name, price, industry, metricsTitle, metricsValue)
 
 
 
+// //for Price/Book ratio companies (Banking)
+// async function scrapeProduct(url_name_price_metrics, url_industry){
+//   const browser = await puppeteer.launch({
+//       // args: ['--no-sandbox',],
+//       // headless: false,
+//       ignoreHTTPSErrors: true,
+  
+//       // add this
+//       executablePath: executablePath(),
+//     });
+//   var page = await browser.newPage();
+//   await page.goto(url_name_price_metrics,{waitUntil: 'networkidle0'});
 
+//   await page.waitForXPath('/html/body/div[2]/section[1]/div[1]/div/h1/span[1]');
+//   const [name] = await page.$x('/html/body/div[2]/section[1]/div[1]/div/h1/span[1]');
+
+//   var text = await name.getProperty('textContent');
+//   const nameText = await text.jsonValue();
+  
+//   await page.waitForXPath('//*[@id="quote_val"]');
+//   const [price] = await page.$x('//*[@id="quote_val"]');
+
+//   text = await price.getProperty('textContent');
+//   const priceText = await text.jsonValue();
+
+//   await page.waitForXPath('/html/body/div[2]/section[2]/div[2]/div[1]/div[3]/div/div[2]/div[1]/div[1]/table/tbody/tr[4]/td/span[1]');
+//   const [metricsTitle] = await page.$x('/html/body/div[2]/section[2]/div[2]/div[1]/div[3]/div/div[2]/div[1]/div[1]/table/tbody/tr[4]/td/span[1]');
+
+//   text = await metricsTitle.getProperty('textContent');
+//   metricsTitleText = await text.jsonValue();
+
+//   await page.waitForXPath('/html/body/div[2]/section[2]/div[2]/div[1]/div[3]/div/div[2]/div[1]/div[1]/table/tbody/tr[4]/td/span[2]/span');
+//   const [metricsValue] = await page.$x('/html/body/div[2]/section[2]/div[2]/div[1]/div[3]/div/div[2]/div[1]/div[1]/table/tbody/tr[4]/td/span[2]/span');
+
+//   text = await metricsValue.getProperty('textContent');
+//   metricsValueText = await text.jsonValue();
+
+//   console.log({nameText});
+//   console.log({priceText});
+//   console.log({metricsTitleText});
+//   console.log({metricsValueText});
+  
+  
+//   await page.goto(url_industry,{waitUntil: 'networkidle0'});
+
+//   await page.waitForXPath('//*[@id="cr_info_mod"]/div[2]/div/div[1]/div[2]/ul/li[2]/div[2]/span[2]');
+//   const [industry] = await page.$x('//*[@id="cr_info_mod"]/div[2]/div/div[1]/div[2]/ul/li[2]/div[2]/span[2]');
+
+//   text = await industry.getProperty('textContent');
+//   const industryText = await text.jsonValue();
+//   console.log({industryText});
+
+// sendToDatabase(nameText, priceText, industryText, metricsTitleText, metricsValueText);
+
+// await browser.close();
+// }
+
+
+
+
+// scrapeProduct("https://www.wsj.com/market-data/quotes/BMO/financials", "https://www.wsj.com/market-data/quotes/BMO/company-people");
+
+
+
+
+//for Price/Book ratio companies (Banking)
 async function scrapeProduct(url_name_price_metrics, url_industry){
   const browser = await puppeteer.launch({
       // args: ['--no-sandbox',],
@@ -162,6 +227,16 @@ async function scrapeProduct(url_name_price_metrics, url_industry){
       executablePath: executablePath(),
     });
   var page = await browser.newPage();
+  await page.goto(url_industry,{waitUntil: 'networkidle0'});
+
+  await page.waitForXPath('//*[@id="cr_info_mod"]/div[2]/div/div[1]/div[2]/ul/li[2]/div[2]/span[2]');
+  const [industry] = await page.$x('//*[@id="cr_info_mod"]/div[2]/div/div[1]/div[2]/ul/li[2]/div[2]/span[2]');
+
+  text = await industry.getProperty('textContent');
+  const industryText = await text.jsonValue();
+  console.log({industryText});
+
+
   await page.goto(url_name_price_metrics,{waitUntil: 'networkidle0'});
 
   await page.waitForXPath('/html/body/div[2]/section[1]/div[1]/div/h1/span[1]');
@@ -176,17 +251,55 @@ async function scrapeProduct(url_name_price_metrics, url_industry){
   text = await price.getProperty('textContent');
   const priceText = await text.jsonValue();
 
-  await page.waitForXPath('/html/body/div[2]/section[2]/div[2]/div[1]/div[3]/div/div[2]/div[1]/div[1]/table/tbody/tr[4]/td/span[1]');
-  const [metricsTitle] = await page.$x('/html/body/div[2]/section[2]/div[2]/div[1]/div[3]/div/div[2]/div[1]/div[1]/table/tbody/tr[4]/td/span[1]');
+  if ({industryText}.industryText == ' Financial Services '){
 
-  text = await metricsTitle.getProperty('textContent');
-  metricsTitleText = await text.jsonValue();
+    console.log("financial services metrics");
 
-  await page.waitForXPath('/html/body/div[2]/section[2]/div[2]/div[1]/div[3]/div/div[2]/div[1]/div[1]/table/tbody/tr[4]/td/span[2]/span');
-  const [metricsValue] = await page.$x('/html/body/div[2]/section[2]/div[2]/div[1]/div[3]/div/div[2]/div[1]/div[1]/table/tbody/tr[4]/td/span[2]/span');
+    await page.waitForXPath('/html/body/div[2]/section[2]/div[2]/div[1]/div[3]/div/div[2]/div[1]/div[1]/table/tbody/tr[4]/td/span[1]');
+    const [metricsTitle] = await page.$x('/html/body/div[2]/section[2]/div[2]/div[1]/div[3]/div/div[2]/div[1]/div[1]/table/tbody/tr[4]/td/span[1]');
+  
+    text = await metricsTitle.getProperty('textContent');
+    metricsTitleText = await text.jsonValue();
+  
+    await page.waitForXPath('/html/body/div[2]/section[2]/div[2]/div[1]/div[3]/div/div[2]/div[1]/div[1]/table/tbody/tr[4]/td/span[2]/span');
+    const [metricsValue] = await page.$x('/html/body/div[2]/section[2]/div[2]/div[1]/div[3]/div/div[2]/div[1]/div[1]/table/tbody/tr[4]/td/span[2]/span');
+  
+    text = await metricsValue.getProperty('textContent');
+    metricsValueText = await text.jsonValue();
+  
+  } else if ({industryText}.industryText == ' Retail/Wholesale '){
 
-  text = await metricsValue.getProperty('textContent');
-  metricsValueText = await text.jsonValue();
+    console.log("retail/wholesale metrics");
+
+    await page.waitForXPath('/html/body/div[2]/section[2]/div[2]/div[1]/div[3]/div/div[2]/div[2]/div[1]/table/tbody/tr[5]/td/span[1]');
+    const [metricsTitle] = await page.$x('/html/body/div[2]/section[2]/div[2]/div[1]/div[3]/div/div[2]/div[2]/div[1]/table/tbody/tr[5]/td/span[1]');
+  
+    text = await metricsTitle.getProperty('textContent');
+    metricsTitleText = await text.jsonValue();
+  
+    await page.waitForXPath('/html/body/div[2]/section[2]/div[2]/div[1]/div[3]/div/div[2]/div[2]/div[1]/table/tbody/tr[5]/td/span[2]/span');
+    const [metricsValue] = await page.$x('/html/body/div[2]/section[2]/div[2]/div[1]/div[3]/div/div[2]/div[2]/div[1]/table/tbody/tr[5]/td/span[2]/span');
+  
+    text = await metricsValue.getProperty('textContent');
+    metricsValueText = await text.jsonValue();
+
+  } else {
+
+    console.log("other industry metrics");
+
+    await page.waitForXPath('/html/body/div[2]/section[2]/div[2]/div[1]/div[3]/div/div[2]/div[1]/div[1]/table/tbody/tr[1]/td/span[1]');
+    const [metricsTitle] = await page.$x('/html/body/div[2]/section[2]/div[2]/div[1]/div[3]/div/div[2]/div[1]/div[1]/table/tbody/tr[1]/td/span[1]');
+  
+    text = await metricsTitle.getProperty('textContent');
+    metricsTitleText = await text.jsonValue();
+  
+    await page.waitForXPath('/html/body/div[2]/section[2]/div[2]/div[1]/div[3]/div/div[2]/div[1]/div[1]/table/tbody/tr[1]/td/span[2]/span');
+    const [metricsValue] = await page.$x('/html/body/div[2]/section[2]/div[2]/div[1]/div[3]/div/div[2]/div[1]/div[1]/table/tbody/tr[1]/td/span[2]/span');
+  
+    text = await metricsValue.getProperty('textContent');
+    metricsValueText = await text.jsonValue();
+
+  }
 
   console.log({nameText});
   console.log({priceText});
@@ -194,15 +307,6 @@ async function scrapeProduct(url_name_price_metrics, url_industry){
   console.log({metricsValueText});
   
   
-  await page.goto(url_industry,{waitUntil: 'networkidle0'});
-
-  await page.waitForXPath('//*[@id="cr_info_mod"]/div[2]/div/div[1]/div[2]/ul/li[2]/div[2]/span[2]');
-  const [industry] = await page.$x('//*[@id="cr_info_mod"]/div[2]/div/div[1]/div[2]/ul/li[2]/div[2]/span[2]');
-
-  text = await industry.getProperty('textContent');
-  const industryText = await text.jsonValue();
-  console.log({industryText});
-
 sendToDatabase(nameText, priceText, industryText, metricsTitleText, metricsValueText);
 
 await browser.close();
@@ -211,4 +315,4 @@ await browser.close();
 
 
 
-scrapeProduct("https://www.wsj.com/market-data/quotes/JPM/financials", "https://www.wsj.com/market-data/quotes/JPM/company-people");
+scrapeProduct("https://www.wsj.com/market-data/quotes/CHDRY/financials", "https://www.wsj.com/market-data/quotes/CHDRY/company-people");
