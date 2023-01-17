6c18885fa6f1dc15ae593a4ef4a712ff5f14b651
Larabbit <pengx091@newschool.edu>
2023-01-17 12:00:08 -0500

    login using same token session

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,21 +6,27 @@ import Nav from './components/Nav.js'
 import Intro from './components/Intro.js'
 import Login from './components/Login/Login.js';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import useToken from './components/userToken.js';
 // import Create from './components/Create.js'
 // import RecordList from './components/RecordList.js'
 import 'bootstrap/dist/css/bootstrap.css';
 
-function setToken(userToken) {
-  sessionStorage.setItem('token', JSON.stringify(userToken));
-}
+// function setToken(userToken) {
+//   sessionStorage.setItem('token', JSON.stringify(userToken));
+// }
 
-function getToken() {
-}
+// function getToken() {
+//   const tokenString = sessionStorage.getItem('token');
+//   const userToken = JSON.parse(tokenString);
+//   return userToken?.token
+// }
 
 
 function App() {
 
-  const token = getToken();
+  // const token = getToken();
+  // const [token, setToken] = useState();
+  const {token, setToken} = useToken();
 
   if(!token) {
     return <Login setToken={setToken} />
